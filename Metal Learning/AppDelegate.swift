0dee817865e5e69b3b80fd0e5a0a59534302a563
Riley Williams <williams.riley@me.com>
2016-05-04 00:19:56 -0400

    Initial Commit

---
+++ Metal Learning/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Metal Learning
+//
+//  Created by Riley Williams on 5/4/16.
+//  Copyright © 2016 Riley Williams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Metal Learning/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Metal Learning
+//
+//  Created by Riley Williams on 5/4/16.
+//  Copyright © 2016 Riley Williams. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
